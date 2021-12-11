2945c87c73b846d6eb610004d969b37ea7377aa4
yashak <yoan@wp.pl>
2021-12-11 17:42:38 +0000

    wikipedia method added

--- HelloWorldLambda/hello_world/app.py
+++ HelloWorldLambda/hello_world/app.py
@@ -1,50 +1,46 @@
-# imported libraries
 import json
-import decimal
+import wikipedia
 
+# prints when function loads
+print('Loading function')
 
 def lambda_handler(event, context):
-    ''' Accepts some US dollar input and returns the amount of change
-        needed to make up that value.
-        :param event: a request that has a JSON input with some "amount" value
-        :return: a response with the correct US change to make up the amount,
-            formatted in JSON as the number of quarters/dimes/nickels/pennies.
-    '''
+    ''' Wikipedia page summarizer.
+        :param event: a request with a wikipedia "entity" that has page information
+        :return: a response that contains the first sentence of a wikipedia page,
+            the response is JSON formatted.'''
     
-    print(event)
-    
-    # check that the request has some input body
     if 'body' in event:
         event = json.loads(event["body"])
+    
+    entity = event["entity"]
+    
+    (''BAD_REQUEST_STATUS = 400
+    ALL_GOOD_STATUS = 200 
+    
+    # Exception handling
+    try:
+        res = wikipedia.summary(entity, sentences=1) # first sentence, result
+        statusCode = ALL_GOOD_STATUS
+    except wikipedia.exceptions.PageError:
+        res= "\nThis word does not exist!\n"
+        statusCode = BAD_REQUEST_STATUS
+    except wikipedia.exceptions.DisambiguationError: 
+        statusCode = BAD_REQUEST_STATUS
+        res = "\nThere are multiple references to this word!\n"
+    except:
+        statusCode = BAD_REQUEST_STATUS
+        res = "\nSorry, Cannot Handle this request!\n"
 
-    # get float "amount"
-    amount = float(event["amount"])
-
-    # calculate the resultant change and store the result (res)
-    res = []
-    coins = [1,5,10,25] # value of pennies, nickels, dimes, quarters
-    coin_lookup = {25: "quarters", 10: "dimes", 5: "nickels", 1: "pennies"}
-
-    # divide the amount*100 (the amount in cents) by a coin value
-    # record the number of coins that evenly divide and the remainder
-    coin = coins.pop()
-    num, rem  = divmod(int(amount*100), coin)
-    # append the coin type and number of coins that had no remainder
-    res.append({num:coin_lookup[coin]})
-
-    # while there is still some remainder, continue adding coins to the result
-    while rem > 0:
-        coin = coins.pop()
-        num, rem = divmod(rem, coin)
-        if num:
-            if coin in coin_lookup:
-                res.append({num:coin_lookup[coin]})
-
-    # format the response as JSON and return the result
+    # print statements
+    print(f"context: {context}, event: {event}")
+    print(f"Response from wikipedia API: {res}")
+       
+    ## TO DO: Format the response as JSON and return the result
     response = {
         "statusCode": "200",
         "headers": { "Content-type": "application/json" },
         "body": json.dumps({"res": res})
     }
-
+    
     return response
