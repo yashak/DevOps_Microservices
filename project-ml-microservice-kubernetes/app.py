05b8a74151e43db3c51e549f5c9f6e1fa1d0da72
yashak <yoan@wp.pl>
2021-12-15 01:44:05 +0100

    project lib versions update + Dockerfile added + run_docker.sh + modified application result logging

--- project-ml-microservice-kubernetes/app.py
+++ project-ml-microservice-kubernetes/app.py
@@ -58,12 +58,18 @@ def predict():
     LOG.info(f"JSON payload: \n{json_payload}")
     inference_payload = pd.DataFrame(json_payload)
     LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
+
     # scale the input
     scaled_payload = scale(inference_payload)
+
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
-    # TO DO:  Log the output prediction value
-    return jsonify({'prediction': prediction})
+    LOG.info(f"Prediction: \n{prediction}")
+
+    json_response = {'prediction': prediction}
+    LOG.info(f"JSON response: \n{json_response}")
+    
+    return jsonify(json_response)
 
 if __name__ == "__main__":
     # load pretrained model as clf
